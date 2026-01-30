22a2d0b8ede95bd1b92e541d7b7c4c65f6f29a17
myreceiptt <nota@endhonesa.com>
2026-01-31 02:43:34 +0700

    Remove Stacks auth session helper

--- lib/stacks-session.ts
+++
@@ -1,5 +0,0 @@
-import { AppConfig, UserSession } from "@stacks/auth";
-
-const appConfig = new AppConfig(["store_write", "publish_data"]);
-
-export const stacksUserSession = new UserSession({ appConfig });
